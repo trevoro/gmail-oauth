b0ab701b1117762f6d003b00534451a51f85a7c9
Trevor Orsztynowicz <trevoro@joyent.com>
2012-07-30 17:12:36 -0700

    Adding xoauth functionality

--- README.md
+++ README.md
@@ -2,29 +2,57 @@
 
 OAuth for Gmail
 
-## Example (Command-line)
+This module makes it easy to generate Oauth (v1) and Xoauth strings for use with
+gmail's IMAP servers.
 
-    var GmailOAuth = require(__dirname + '../lib/index');
+# Installation
 
-    GmailOAuth.getRequestToken(function (error, results) {
-      if (error) {
-        console.log(error);
-        process.exit(1);
-      }
+    $ npm install gmail-oauth
+
+# API
+
+    var client = require(__dirname + '../lib/index').createClient();
+    
+    client.getRequestToken('johnnytoothpick@gmail.com', function (error, results) {
+      if (error) 
+        throw new Error(error);
+
+      // get the user to go to the authorization URL...
 
       process.stdin.resume();
       process.stdin.setEncoding('utf8');
       process.stdout.write('Enter verification code: ');
 
       process.stdin.once('data', function (text) {
-        var verifier = text.trim();
-        GmailOAuth.getAccessToken(results, verifier, function(error, results) {
-          if (error) { 
-            process.exit(1);
-            console.log(error);
-          } else {
-            console.log(results);
-          }
+        var code = text.trim();
+      
+        client.getAccessToken(results, code, function(error, results) {
+          if (error)  
+            throw new Error(error);
+     
+          console.log(JSON.stringify(results, null, 2));
         });
       });
     });
+
+
+## GmailOauth API
+
+### gmailOauth.createClient([options]);
+
+Create a new oauth client.
+
+options is an object which may specify:
+
+- consumerKey: the consumerKey value to pass to Gmail/Google
+- consumerSecret: the consumerSecret value to pass to Gmail/Google
+- callbackUrl: which callbackUrl to use for this client. Defaults to 'oob'
+
+## Class: gmailOauth.client;
+
+### client.getRequestToken(user, callback)
+
+### client.getAccessToken(request, verifier, callback)
+
+### client.xoauthString(user, accessToken, accessTokenSecret)
+
--- example.js
+++ example.js
@@ -1,35 +1,52 @@
 var GmailOAuth = require('./lib/index');
 var client = GmailOAuth.createClient();
 
-process.stdin.setEncoding('utf8');
-process.stdin.resume();
-process.stdout.write('Enter email address: ');
 
-process.stdin.once('data', function (text) {
-  var user = text.trim();
+var _stdin = process.stdin;
+var _stdout = process.stdout;
 
+_stdin.setEncoding('utf8');
+
+var ask = function(question, callback) {
+  process.stdout.write(question + ': ');
+  process.stdin.resume();
+  
+  process.stdin.once('data', function (text) {
+    var answer = text.trim();
+    process.stdin.pause();
+    callback(answer);
+  });
+};
+
+ask('enter your email address', function(user) {
   client.getRequestToken(user, function (error, results) {
+
     if (error) 
       throw new Error (error);
-
+    
+    console.log('--------------------------------');
     console.log('Please visit the following URL and grant permission');
     console.log(results.authorizeUrl);
 
-    process.stdin.resume();
-    process.stdout.write('Enter verification code: ');
-
-    process.stdin.once('data', function (text) {
-      var verifier = text.trim();
-      client.getAccessToken(results, verifier, function(error, results) {
+    ask('Enter verification code', function(code) {
+      client.getAccessToken(results, code, function(error, results) {
         if (error) { 
           console.log(error);
           process.exit(1);
         } else {
-          console.log(results);
+          var xoauthString = client.getXoauthString(
+            user, 
+            results.accessToken, 
+            results.accessTokenSecret);
+
+          console.log('\n--------------------------------');
+          console.log("access token:  %s", results.accessToken);
+          console.log("access secret: %s", results.accessTokenSecret);
+          console.log("xoauth string: %s", xoauthString);
+          process.exit(0);
         }
-        process.exit(0);
       });
     });
-  });
 
+  });
 });
